ebb363ef4c30f04139937d5300fb98dceb72625b
nyel-101 <dandevjr@gmail.com>
2025-02-16 12:38:53 +0000

    feat(auth): sanitize session store

--- fury/lib/index.ts
+++ fury/lib/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_117623c146b14d27978a401b649d11d3 = () => {
-  return '2026-05-19T19:25:47Z';
+const node_8d82ce6e8cf6466abb27aa7356c3e2ab = () => {
+  return '2025-02-16T12:38:53Z';
 };
