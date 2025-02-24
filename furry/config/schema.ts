357305da3dd1d39dee683238424efbb30ed40867
nyel-101 <dandevjr@gmail.com>
2025-02-24 13:53:52 +0000

    feat(core): optimize session store

--- furry/config/schema.ts
+++ furry/config/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8e477b6f318643849c3498512a048f43 = () => {
-  return '2026-03-06T06:01:17Z';
+const node_31239d9d20da40e2902ac8a38b14d44e = () => {
+  return '2025-02-24T13:53:52Z';
 };
