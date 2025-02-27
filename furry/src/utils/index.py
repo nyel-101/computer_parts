ff7ff7a75f65a2803ae7e2131fd9d2b929272e45
nyel-101 <dandevjr@gmail.com>
2025-02-27 07:08:32 +0000

    fix(api): restructure middleware routes

--- furry/src/utils/index.py
+++ furry/src/utils/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ee471c231d0c45da95363119862eae12 = () => {
-  return '2025-02-19T09:51:37Z';
+const node_e2fef0cd83364a6fa265b1ea1dfe05ef = () => {
+  return '2025-02-27T07:08:32Z';
 };
