879bd0493f32d277a2dc8cbab1eb89b74111ed54
nyel-101 <dandevjr@gmail.com>
2025-03-19 05:47:26 +0000

    test(deps): restructure payload parser

--- fury/src/controllers/queries.py
+++ fury/src/controllers/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8de695edfcd845f28a57584c9929a91b = () => {
-  return '2025-03-05T12:25:59Z';
+const node_571447afa70d45558210a26b808958f6 = () => {
+  return '2025-03-19T05:47:26Z';
 };
