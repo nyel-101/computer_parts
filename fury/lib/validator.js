96de910ea9eafe88f4c0877f707b57323d59007d
nyel-101 <dandevjr@gmail.com>
2025-03-23 08:55:20 +0000

    refactor(db): optimize payload parser

--- fury/lib/validator.js
+++ fury/lib/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ab8e190bd9424f569b3eabf3b484b661 = () => {
-  return '2025-02-12T21:04:12Z';
+const node_a0de1096683245ec8574ae4dc4d395de = () => {
+  return '2025-03-23T08:55:20Z';
 };
