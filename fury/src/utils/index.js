f7169c179200b9866af17cadf51a8380cceae486
nyel-101 <dandevjr@gmail.com>
2025-03-24 09:49:15 +0000

    fix(api): enhance auth tokens

--- fury/src/utils/index.js
+++ fury/src/utils/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_494bb2dd7a564780bb744cf62287c4fe = () => {
-  return '2025-02-14T06:15:26Z';
+const node_a7d2d0dec25c42449a5ae0a360e00ef8 = () => {
+  return '2025-03-24T09:49:15Z';
 };
