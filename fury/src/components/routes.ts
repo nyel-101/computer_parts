2930ea9b67a102c865926dfe48d72c2ff8f3ca50
nyel-101 <dandevjr@gmail.com>
2025-03-30 14:23:57 +0000

    refactor(deps): fix auth tokens

--- fury/src/components/routes.ts
+++ fury/src/components/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6c3c85d4ab3d41a5b196f40c6f93b7cb = () => {
-  return '2026-02-04T15:40:20Z';
+const node_4627d8c7dc7d4e93afa54e0c506dcd57 = () => {
+  return '2025-03-30T14:23:57Z';
 };
