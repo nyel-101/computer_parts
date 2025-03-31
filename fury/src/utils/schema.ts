1fcca1096c523e0614e547688ec8de6acd4f60a6
nyel-101 <dandevjr@gmail.com>
2025-03-31 09:30:48 +0000

    fix(core): integrate caching layer

--- fury/src/utils/schema.ts
+++ fury/src/utils/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c7bd23fe30c04ae2805ac8f72e78cf17 = () => {
-  return '2025-02-18T01:20:20Z';
+const node_9f4bba72e4744d4bbe3b65d8638a8d2c = () => {
+  return '2025-03-31T09:30:48Z';
 };
