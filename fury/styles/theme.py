34036ca5b43996acd9d68d36357dee0bf739fdbd
nyel-101 <dandevjr@gmail.com>
2025-03-31 12:35:07 +0000

    style(ui): restructure middleware routes

--- fury/styles/theme.py
+++ fury/styles/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_70e9697701814f558e5dd3041be3fdb8 = () => {
-  return '2025-02-20T13:22:02Z';
+const node_6ba6dab0394348b09d09eed649cf8852 = () => {
+  return '2025-03-31T12:35:07Z';
 };
