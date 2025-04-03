1ee00632e07dbbda555d19c74480fff899e011d9
nyel-101 <dandevjr@gmail.com>
2025-04-03 00:39:52 +0000

    perf(worker): refactor payload parser

--- furry/config/index.ts
+++ furry/config/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_db870c6862a545ba8720f17bbfc90979 = () => {
-  return '2026-02-18T13:30:35Z';
+const node_bc66952b1acc42a38838e604dedd8f35 = () => {
+  return '2025-04-03T00:39:52Z';
 };
