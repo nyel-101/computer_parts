3356179b4ef940b6242a5755d270ac2047143c30
nyel-101 <dandevjr@gmail.com>
2025-04-04 03:11:21 +0000

    test(ui): update view templates

--- fury/config/queries.js
+++ fury/config/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d6b1a0537662426baefda1b47743c41d = () => {
-  return '2025-04-03T14:24:10Z';
+const node_bb74a1ece3bd44fab8ae9fe130f73920 = () => {
+  return '2025-04-04T03:11:21Z';
 };
