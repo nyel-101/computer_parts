3ba2832c282a58065962ef2a0aadd2bc4c510589
nyel-101 <dandevjr@gmail.com>
2025-04-04 08:20:40 +0000

    fix(worker): implement middleware routes

--- fury/src/controllers/queries.py
+++ fury/src/controllers/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_571447afa70d45558210a26b808958f6 = () => {
-  return '2025-03-19T05:47:26Z';
+const node_24260d7aaeac4da18e3537a27e68760d = () => {
+  return '2025-04-04T08:20:40Z';
 };
