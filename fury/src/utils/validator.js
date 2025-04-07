24b3c6eb81297d4829b6f90ee711853c90bbfad6
nyel-101 <dandevjr@gmail.com>
2025-04-07 04:53:29 +0000

    fix(api): sanitize database index

--- fury/src/utils/validator.js
+++ fury/src/utils/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_12d7f9481189483b83a7e8aa86c69f4d = () => {
-  return '2025-03-23T06:57:57Z';
+const node_78b2ea9fcd5f469b832c2b80e96992c0 = () => {
+  return '2025-04-07T04:53:29Z';
 };
