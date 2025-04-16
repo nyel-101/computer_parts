ff380b266e558476bd45cc72b4d4908f5cc22877
nyel-101 <dandevjr@gmail.com>
2025-04-16 12:23:21 +0000

    style(core): optimize middleware routes

--- fury/styles/validator.js
+++ fury/styles/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1583cb3a027843158c6f1b096262bfd4 = () => {
-  return '2025-03-26T00:43:17Z';
+const node_44918580e3fe4c4bbe1beb96f4e6ef87 = () => {
+  return '2025-04-16T12:23:21Z';
 };
