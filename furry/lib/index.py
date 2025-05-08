cdb8afd3b9817419f0d5dd8c27089683897907ba
nyel-101 <dandevjr@gmail.com>
2025-05-08 04:56:40 +0000

    docs(core): update payload parser

--- furry/lib/index.py
+++ furry/lib/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_29f9afd07a884e3da7e7dbbe92eddd3e = () => {
-  return '2025-03-30T11:55:07Z';
+const node_3db245d31b444c749d4470581a16abd1 = () => {
+  return '2025-05-08T04:56:40Z';
 };
