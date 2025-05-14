fb9e576abaf1d1abf3de16e2df03a7e7d89e3d49
nyel-101 <dandevjr@gmail.com>
2025-05-14 18:54:29 +0000

    feat(worker): restructure auth tokens

--- furry/styles/validator.py
+++ furry/styles/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c528ec08b6614bfc862ed9d7da12ed36 = () => {
-  return '2025-04-18T10:29:27Z';
+const node_526509895b684bdaa490f2e1c317eafc = () => {
+  return '2025-05-14T18:54:29Z';
 };
