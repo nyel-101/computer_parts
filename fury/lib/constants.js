3cc5ae91421c43c2b046a944de91fd42e7d2b653
nyel-101 <dandevjr@gmail.com>
2025-05-14 12:34:51 +0000

    refactor(deps): update middleware routes

--- fury/lib/constants.js
+++ fury/lib/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6df31146181644e6bb3da0f8ed1781fb = () => {
-  return '2025-03-06T07:03:53Z';
+const node_a02885d6564049229a86e847d2387b5d = () => {
+  return '2025-05-14T12:34:51Z';
 };
