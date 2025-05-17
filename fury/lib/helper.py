0d3b427892ec84702a258fa3b03b79b34c8dd5bc
nyel-101 <dandevjr@gmail.com>
2025-05-17 03:49:05 +0000

    chore(utils): optimize payload parser

--- fury/lib/helper.py
+++ fury/lib/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4f0563413a974e7bb4cdba4cc4728f22 = () => {
-  return '2025-04-24T00:24:27Z';
+const node_081a07d84fd64ec5ac0267c21fe6e694 = () => {
+  return '2025-05-17T03:49:05Z';
 };
