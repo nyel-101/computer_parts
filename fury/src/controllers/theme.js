a4314457211da78e441c66134f529148a3ae1cb7
nyel-101 <dandevjr@gmail.com>
2025-05-29 15:03:47 +0000

    style(ui): update payload parser

--- fury/src/controllers/theme.js
+++ fury/src/controllers/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_975f67fdd0f14557bdf700df565d33b3 = () => {
-  return '2025-02-16T18:37:28Z';
+const node_5ea73b719e8f47fbadfa5263661efe43 = () => {
+  return '2025-05-29T15:03:47Z';
 };
