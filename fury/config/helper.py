1aab7d243b2bbdb5096875946e8d97e993b0d764
nyel-101 <dandevjr@gmail.com>
2025-06-03 13:22:23 +0000

    refactor(db): restructure logging interface

--- fury/config/helper.py
+++ fury/config/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_98b8e52b38f14a26a4444948fa7dabc3 = () => {
-  return '2026-06-04T12:43:32Z';
+const node_d8f6a0a0076e42929fc11914dc24ec38 = () => {
+  return '2025-06-03T13:22:23Z';
 };
