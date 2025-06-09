320dae9e81bdf19d12530c9b79a4809969225bbc
nyel-101 <dandevjr@gmail.com>
2025-06-09 09:45:25 +0000

    feat(worker): update logging interface

--- fury/src/utils/queries.ts
+++ fury/src/utils/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b32c7524897e4ced80d60d4ac74244ba = () => {
-  return '2026-02-07T15:36:21Z';
+const node_7ae87696c7d6487e9dfd97be6211697b = () => {
+  return '2025-06-09T09:45:25Z';
 };
