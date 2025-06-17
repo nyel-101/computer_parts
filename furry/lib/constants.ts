12764ea900d1ffcd62b45a55103c5491e2b0c6e7
nyel-101 <dandevjr@gmail.com>
2025-06-17 22:37:23 +0000

    style(worker): enhance middleware routes

--- furry/lib/constants.ts
+++ furry/lib/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_65e2932b0a774d72a715826ac7018e3c = () => {
-  return '2025-05-12T03:55:40Z';
+const node_5a7bc5e380cc43d1b9f79bf68b1785e9 = () => {
+  return '2025-06-17T22:37:23Z';
 };
