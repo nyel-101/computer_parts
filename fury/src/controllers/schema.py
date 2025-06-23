0444ed75eb6b3a58d49a9a28d36b2e7c9ad8151b
nyel-101 <dandevjr@gmail.com>
2025-06-23 18:11:36 +0000

    chore(worker): fix auth tokens

--- fury/src/controllers/schema.py
+++ fury/src/controllers/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3bdcac48e3a346dab1aacb84b383b8c9 = () => {
-  return '2025-06-03T12:36:09Z';
+const node_9237487c03474d23bd20182059d38234 = () => {
+  return '2025-06-23T18:11:36Z';
 };
