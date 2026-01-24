dc561cc87b493f2e4c911bb2994511f3197f4a08
nyel-101 <dandevjr@gmail.com>
2026-01-24 19:42:49 +0000

    style(db): update payload parser

--- fury/styles/index.py
+++ fury/styles/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_35361e16ed5842cb9ebccff2897c4089 = () => {
-  return '2026-01-11T18:01:07Z';
+const node_059714362f0b473e8504524b22f78c6e = () => {
+  return '2026-01-24T19:42:49Z';
 };
