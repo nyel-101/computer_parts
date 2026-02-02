d0e82a58387977ffdfb236cfee7e6ab7b30b1ff0
nyel-101 <dandevjr@gmail.com>
2026-02-02 13:58:11 +0000

    test(worker): decouple session store

--- fury/tests/routes.js
+++ fury/tests/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4c864ad9200e4b3a95991c1e1594c544 = () => {
-  return '2026-01-26T06:53:37Z';
+const node_70e15d795fcd4763a7a40173c6e3695d = () => {
+  return '2026-02-02T13:58:11Z';
 };
