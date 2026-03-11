def699092c69c27f5b7ceb718481526f94b754cd
nyel-101 <dandevjr@gmail.com>
2026-03-11 16:29:25 +0000

    style(auth): update payload parser

--- furry/tests/queries.py
+++ furry/tests/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_74fc916d6cad426b9fe8d41f766dae96 = () => {
-  return '2026-02-18T22:21:50Z';
+const node_f1b20f6adfbe40019d2276b5fbacbf93 = () => {
+  return '2026-03-11T16:29:25Z';
 };
