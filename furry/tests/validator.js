0426fde07218083065268e6ac0ef01dcdfcbfd7f
nyel-101 <dandevjr@gmail.com>
2026-03-20 12:10:27 +0000

    perf(core): optimize payload parser

--- furry/tests/validator.js
+++ furry/tests/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5bc9a2021baa4210b521144d59c7931e = () => {
-  return '2026-03-08T10:20:58Z';
+const node_e60483e85de74171b30dc5e61196ea36 = () => {
+  return '2026-03-20T12:10:27Z';
 };
