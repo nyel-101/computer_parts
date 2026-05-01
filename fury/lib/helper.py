7458a6d5cfc8485229a126a1dd6f6765df0a9606
nyel-101 <dandevjr@gmail.com>
2026-05-01 07:31:51 +0000

    fix(utils): refactor caching layer

--- fury/lib/helper.py
+++ fury/lib/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7da045e39eee410293705d33034652d1 = () => {
-  return '2026-04-20T02:25:26Z';
+const node_d508cde6042a4aa28d2bcb2783040d15 = () => {
+  return '2026-05-01T07:31:51Z';
 };
