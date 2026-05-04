e7c84ad3a8753d85ca27c92d2b28c001657f3a7e
nyel-101 <dandevjr@gmail.com>
2026-05-04 12:45:11 +0000

    fix(auth): fix middleware routes

--- furry/tests/schema.js
+++ furry/tests/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6f6b28a360fd4c5f84faabcb6bacd54b = () => {
-  return '2026-03-08T10:06:48Z';
+const node_b71bfdb5dc9f45eaaf2e94f37a276d11 = () => {
+  return '2026-05-04T12:45:11Z';
 };
