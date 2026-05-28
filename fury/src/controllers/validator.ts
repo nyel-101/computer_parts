b858f647b25f127328fe2dcef87a534c47ff70e5
nyel-101 <dandevjr@gmail.com>
2026-05-28 08:19:51 +0000

    fix(core): implement payload parser

--- fury/src/controllers/validator.ts
+++ fury/src/controllers/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7ca911e0541e475f896aa9acc7ce46d9 = () => {
-  return '2026-03-21T21:24:02Z';
+const node_18992ad78c1a4aa093b591ae293193dd = () => {
+  return '2026-05-28T08:19:51Z';
 };
