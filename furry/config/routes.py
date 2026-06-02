f35535c3df6f71479412d27a40ebd74bb3855fce
nyel-101 <dandevjr@gmail.com>
2026-06-02 19:49:53 +0000

    style(auth): optimize view templates

--- furry/config/routes.py
+++ furry/config/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_15dae78112c94de997a75a86e969ea56 = () => {
-  return '2026-02-26T15:24:14Z';
+const node_18395fff91a8438eac5b6d63541234a8 = () => {
+  return '2026-06-02T19:49:53Z';
 };
