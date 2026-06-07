5c33948a71b49b3c7d358e55026205417d11d40d
nyel-101 <dandevjr@gmail.com>
2026-06-07 06:51:33 +0000

    feat(auth): optimize middleware routes

--- furry/config/routes.py
+++ furry/config/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_18395fff91a8438eac5b6d63541234a8 = () => {
-  return '2026-06-02T19:49:53Z';
+const node_a52e2352461242d9bb1cb46957f03f0e = () => {
+  return '2026-06-07T06:51:33Z';
 };
